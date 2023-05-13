558911982f49f93675beb5883af2e8ee59fb4d5b
Wiktoria <wikiel.wiktoria@gmail.com>
2023-05-13 13:22:47 +0200

    Update +page.svelte

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -38,52 +38,59 @@
 >
 	<h2 class="text-2xl md:text-4xl">Atrakcyjne rabaty dla stałych klientów</h2>
 	<table class="table-price">
-		<tr> <th>POJAZDY</th> <th>Cena</th> </tr>
-		<tr> <td>zewnętrzne ręczne mycie pojazdu</td> <td>od 79 zł</td> </tr>
-		<tr> <td>wewnętrzne sprzątanie pojazdu</td> <td>od 99 zł</td> </tr>
-		<tr> <td>czyszczenie skórzanej tapicerki</td><td>od 249 zł</td> </tr>
-		<tr> <td>pranie kompletu tapicerki</td> <td>od 199 zł</td> </tr>
-		<tr> <td>pranie wykładziny + dywaników</td> <td>od 149 zł</td> </tr>
-		<tr> <td>pranie komory bagażnika </td><td>od 49 zł</td> </tr>
-		<tr> <td>pranie podsufitki</td><td>od 99 zł</td> </tr>
-		<tr> <td>impregncacja tapicerki skórzanej</td> <td>od 199 zł</td> </tr>
-		<tr> <td>impregnacja tapicerki materiałowej</td> <td>od 149 zł</td> </tr>
-		<tr> <td>impregnacja powłoki lakierniczej ciekłym woskiem</td> <td>od 300 zł</td> </tr>
-		<tr> <td>ozonowanie/dezynfekcja</td><td>od 99 zł</td> </tr>
+		<tr> <th>Mycie kostki i ogrodzeń</th> <th>Cena</th> </tr>
+		<tr> <td>Chemiczne mycie kostki brukowej/chodnikowej</td> <td>od 7 zł/m²</td> </tr>
+		<tr> <td>Sezonowa impregnacja kostki brukowej/chodnikowej</td> <td>od 5 zł/m²</td> </tr>
+		<tr> <td>Wielosezonowa impregnacja kostki brukowej/chodnikowej</td><td>od 10 zł/m²</td> </tr>
+		<tr> <td>Usuwanie dodatkowych zabrudzeń np. plamy olejowe,rdza</td> <td>od 20 zł/m²</td> </tr>
+		<tr> <td>Uzupełnianie spoin piaskiem</td> <td>od 5 zł/m²</td> </tr>
+		<tr> <td>Mycie ogrodzeń</td><td>od 9 zł/mb</td> </tr>
 	</table>
 
 	<table class="table-price">
-		<tr> <th>BUDYNKI</th> <th>Cena</th> </tr>
-		<tr> <td>mycie elewacji i dachów </td> <td>od 10 zł/m²</td> </tr>
-		<tr> <td>mycie okien</td><td>od 15 zł</td> </tr>
-		<tr> <td>czyszczenie rynien</td> <td>od 10 zł/m²</td> </tr>
-		<tr> <td>mycie witryn sklepowych</td> <td>od 7 zł/m²</td> </tr>
-		<tr> <td>impregnacja elewacji/dachów</td><td>od 10 zł/m²</td> </tr>
-		<tr> <td>impregnacja okien</td> <td>od 49 zł/m²</td> </tr>
-		<tr> <td>czyszczenie zabrudzeń pobudowlanych</td> <td>od 40 zł/m²</td> </tr>
-		<tr> <td>usuwanie graffiti </td><td>od 70 zł/m²</td> </tr>
-		<tr> <td>zabezpieczenie antygrafiti </td><td>od 50 zł/m²</td> </tr>
+		<tr> <th>Mycie Budynków</th> <th>Cena</th> </tr>
+		<tr> <td>Chemiczne mycie elewacji</td> <td>od 10 zł/m²</td> </tr>
+		<tr> <td>Chemiczne mycie dachów</td><td>od 12 zł/m²</td> </tr>
+		<tr> <td>Sezonowa impregnacja elewacji</td> <td>od 5 zł/m²</td> </tr>
+		<tr> <td>Wielosezonowa impregnacja elewacji</td> <td>od 10 zł/m²</td> </tr>
+		<tr> <td>Sezonowa impregnacja dachów</td><td>od 5 zł/m²</td> </tr>
+		<tr> <td>Mycie okien</td> <td>od 15 zł/szt</td> </tr>
+		<tr> <td>Czyszczenie rynien</td> <td>od 6 zł/mb</td> </tr>
+		<tr> <td>Mycie witryn sklepowych </td><td>od 6 zł/m²</td> </tr>
+		<tr> <td>Impregnacja okien</td><td>od 29 zł/m²</td> </tr>
+		<tr> <td>Czyszczenie zabrudzeń pobudowlanych</td><td>od 40 zł/m²</td> </tr>
+		<tr> <td>Usuwanie graffiti </td><td>od 70 zł/m²</td> </tr>
+		<tr> <td>Zabezpieczenie antygrafiti </td><td>od 50 zł/m²</td> </tr>
 	</table>
 
 	<table class="table-price">
-		<tr> <th> WNĘTRZA</th> <th>Cena</th> </tr>
-		<tr> <td>pranie dywanów i wykładzin z krótkim włosiem</td> <td>od 15 zł/m²</td> </tr>
-		<tr> <td>pranie narożników</td><td>od 149 zł</td> </tr>
-		<tr> <td>pranie kanap</td> <td>od 99 zł</td> </tr>
-		<tr> <td>pranie foteli </td> <td>od 49 zł</td> </tr>
-		<tr> <td>pranie poduszek </td><td>od 10 zł</td> </tr>
-		<tr> <td>czyszczenie krzeseł </td><td>od 15 zł</td> </tr>
-		<tr> <td>sprzątanie pomieszczeń</td> <td>od 5 zł/m²</td> </tr>
-		<tr> <td>impregnacja wykładzin</td><td>od 2 zł/m²</td> </tr>
+		<tr> <th> Czyszczenie róznego rodzaju pojazdów</th> <th>Cena</th> </tr>
+		<tr> <td>Zewnętrzne ręczne mycie pojazdu</td> <td>od 79 zł</td> </tr>
+		<tr> <td>Wewnętrzne sprzątanie pojazdu</td><td>od 99 zł</td> </tr>
+		<tr> <td>Czyszczenie skórzanej tapicerki</td> <td>od 249 zł</td> </tr>
+		<tr> <td>Pranie kompletu tapicerki</td> <td>od 199 zł</td> </tr>
+		<tr> <td>Pranie wykładziny+dywaników</td><td>od 149 zł</td> </tr>
+		<tr> <td>Pranie komory bagażnika</td><td>od 49 zł</td> </tr>
+		<tr> <td>Pranie podsufitki</td> <td>od 99 zł</td> </tr>
+		<tr> <td>Impregnacja kompletu tapicerki skórzanej</td><td>od 199 zł</td> </tr>
+		<tr> <td>Impregnacja kompletu tapicerki materiałowej</td><td>od 149 zł</td> </tr>
+		<tr> <td>Impregnacja powłoki lakierniczej ciekłym woskiem</td><td>od 99 zł</td> </tr>
+		<tr> <td>Ozonowanie/Defyznfekcja</td><td>od 99 zł</td> </tr>
 	</table>
 
 	<table class="table-price">
-		<tr> <th>POZOSTAŁE</th> <th>Cena</th> </tr>
-		<tr> <td>kostki brukowej/chodnikowej</td> <td>od 7 zł/m²</td> </tr>
-		<tr> <td>mycie ogrodzeń</td> <td>od 9 zł/m²</td> </tr>
-		<tr> <td>impregnacja kostki brukowej/płyt chodnikowych </td><td>od 10 zł/m²</td> </tr>
-		<tr> <td>usuwanie dodatkowych zabrudzeń</td> <td>od 20 zł/m²</td> </tr>
-		<tr> <td>uzupełnianie spoin piaskiem</td> <td>od 4 zł/m²</td> </tr>
+		<tr> <th>Czyszczenie wnętrz</th> <th>Cena</th> </tr>
+		<tr> <td>Pranie dywanów</td> <td>od 15 zł/m²</td> </tr>
+		<tr> <td>Pranie wykładzin</td> <td>od 12 zł/m²</td> </tr>
+		<tr> <td>Pranie narożników</td><td>od 149 zł/szt</td> </tr>
+		<tr> <td>Pranie kanap</td><td>od 99 zł/szt</td> </tr>
+		<tr> <td>Pranie foteli</td><td>od 49 zł/szt</td> </tr>
+		<tr> <td>Pranie poduszek</td><td>od 10 zł/szt</td> </tr>
+		<tr> <td>Czyszczenie krzeszeł</td><td>od 15 zł/szt</td> </tr>
+		<tr> <td>Sprzątanie pomieszczeń biurowych</td><td>od 5 zł/m²</td> </tr>
+		<tr> <td>Sprzątanie mieszkań</td><td>od 5 zł/m²</td> </tr>
+		<tr> <td>Sprzątanie magazynów</td><td>od 2 zł/m²</td> </tr>
+		<tr> <td>Impregnacja wykładzin</td><td> 7 zł/m²</td> </tr>
 	</table>
 
 	<p class="pb-24">
