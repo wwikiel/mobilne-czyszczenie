4f625ec915b62c638a50d34ee4677bd8a555c5d4
Wiktoria <123811016+wwikiel@users.noreply.github.com>
2023-05-13 23:00:59 +0200

    Update +page.svelte

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -12,7 +12,6 @@
 	import img10 from '$lib/assets/gallery/img-10.jpg';
 	import img11 from '$lib/assets/gallery/img-11.jpg';
 	import img12 from '$lib/assets/gallery/img-12.jpg';
-	import img50 from '$lib/assets/gallery/img-50.jpg';
 </script>
 
 <Navbar let:hidden let:toggle color="none" navClass="bg-black px-2 sm:px-4 py-2.5 w-full">
@@ -63,6 +62,5 @@
 		<img src={img10} class="w-full h-72 object-cover" alt="img10" />
 		<img src={img11} class="w-full h-72 object-cover" alt="img11" />
 		<img src={img12} class="w-full h-72 object-cover" alt="img12" />
-		<img src={img50} class="w-full h-72 object-cover" alt="img13" />
 	</div>
 </div>
